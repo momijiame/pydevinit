f2e1a3a5e36f2c672036786d1afa995839bca619
momijiame <amedama.ginmokusei@gmail.com>
2015-11-21 15:40:13 +0900

    サポートするバージョンに Python 3.4, 3.5 を追加する

--- setup.py
+++ setup.py
@@ -56,6 +56,8 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
             'Development Status :: 3 - Alpha',
             'License :: OSI Approved :: Apache Software License',
             'Intended Audience :: Developers',
